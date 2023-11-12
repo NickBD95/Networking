e7800b2b3a7a13845e56d621109d1a81c62b272b
Nickolai Grachev <nickbd95@gmail.com>
2023-11-12 17:33:40 +0300

    Initial Commit

---
+++ Networking/Models/Course.swift
@@ -0,0 +1,24 @@
+//
+//  Course.swift
+//  Networking
+//
+//  Created by brubru on 09.11.2023.
+//  Copyright © 2023 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+struct Course: Decodable {
+	let name: String?
+	let link: String?
+	let imageUrl: String?
+	let number_of_lessons: Int?
+	let number_of_tests: Int?
+}
+
+struct SwiftbookInfo: Decodable {
+	let courses: [Course]?
+	let websiteDescription: String?
+	let websiteName: String?
+}
+
